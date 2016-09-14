2016a7cb50463f4610f6bdda031753e0d94e7203
Steven Bateman <BatemanVO@gmail.com>
2016-09-14 07:22:33 -0700

    Dynamically update the DOM. Created a template in the index.html, then created a constructor function, a toHtml method for the constructor's prototype, and performed some array methods to properly append the templated items to the DOM.

---
+++ js/render_sources.js
@@ -0,0 +1,24 @@
+var allInterests = [];
+
+function Interest(interest) {
+  this.interestName = interest.interestName;
+  this.interestImg = interest.interestImg;
+  this.interestText = interest.interestText;
+}
+
+Interest.prototype.toHtml = function() {
+  var $interestTemplate = $('.template').clone();
+  $interestTemplate.find('h2').text(this.interestName);
+  $interestTemplate.find('img').attr('src', this.interestImg);
+  $interestTemplate.find('p').text(this.interestText);
+  $interestTemplate.removeClass('template');
+  return $interestTemplate;
+};
+
+interests.forEach(function(interest) {
+  allInterests.push(new Interest(interest));
+});
+
+allInterests.forEach(function(interest) {
+  $('#interests').append(interest.toHtml());
+});
