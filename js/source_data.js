b2f099d88523b3e3c61868bff08cfb063717d3c7
Steven Bateman <BatemanVO@gmail.com>
2016-09-14 07:07:10 -0700

    Created file with array of 3 objects.

---
+++ js/source_data.js
@@ -0,0 +1,17 @@
+var interests = [
+  {
+    interestName: 'Northwall',
+    interestImg: 'http://placehold.it/300x300',
+    interestText: 'Northwall is an SNES-style RPG that\'s free to download and play, designed and built by Steven.'
+  },
+  {
+    interestName: 'Voice Over',
+    interestImg: 'http://placehold.it/300x300',
+    interestText: 'Steven has produced audiobooks for Audible.com and provided voices for several small projects, including two games on Steam.',
+  },
+  {
+    interestName: 'Magic',
+    interestImg: 'http://placehold.it/300x300',
+    interestText: 'Magic: The Gathering is a collectible trading card game. Steven and a few other members of Code Fellows play a variant called EDH or Commander.'
+  }
+];
