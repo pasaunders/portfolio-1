4ab04644e7bc514525c4f44aa0e5964e8fd690b2
Steven Bateman <BatemanVO@gmail.com>
2016-09-14 20:12:46 -0700

    Moved BusMall and Embody to source_data objects.

--- js/source_data.js
+++ js/source_data.js
@@ -19,5 +19,19 @@ var interests = [ //eslint-disable-line
     interestText: 'Magic: The Gathering is a collectible trading card game. Steven and a few other members of Code Fellows play a variant called EDH or Commander.',
     interestUrl: 'http://tappedout.net/mtg-decks/10-08-16-Ptu-werewolves/',
     lastWorked: '2016-09-10'
+  },
+  {
+    interestName: 'BusMall',
+    interestImg: 'http://placehold.it/300x300',
+    interestText: 'BusMall is a focus group app that lets the user select from 1 of 3 images which product they like the most, then graphs the results.',
+    interestUrl: 'http://batemanvo.github.io/bus-mall/index.html',
+    lastWorked: '2016-09-01'
+  },
+  {
+    interestName: 'Embody',
+    interestImg: 'http://placehold.it/300x300',
+    interestText: 'Embody is an app that allows physical therapists and their patients to better track their exercise homework and feedback about their exercises.',
+    interestUrl: 'http://batemanvo.github.io/embody/index.html',
+    lastWorked: '2016-09-09'
   }
 ];
